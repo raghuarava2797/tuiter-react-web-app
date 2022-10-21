cac840c09d7b2edbc31166a123addfda5a85709a
aravaraghu <97917143+aravaraghu@users.noreply.github.com>
2022-10-21 09:16:10 -0400

    final code commit for a6 assignment

--- src/labs/index.js
+++ src/labs/index.js
@@ -2,14 +2,11 @@ import Assignment6 from "./a6";
 import Nav from "../nav";
 
 function Labs() {
-    return ( <
-        div >
-        <
-        Nav / >
-        <
-        Assignment6 / >
-        <
-        /div>
+    return ( 
+        <div>
+        <Nav/>
+        <Assignment6/>
+        </div>
     );
 }
 export default Labs;
--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 
 function Nav() {
-    return ( <
-        div >
-        <
-        Link to = "/" > Labs < /Link> | <
-        Link to = "/hello" > Hello < /Link> | <
-        Link to = "/tuiter" > Tuiter < /Link> < /
-        div >
+    return ( 
+        <div>
+            <Link to = "/"> Labs </Link> | 
+            <Link to = "/hello"> Hello </Link> | 
+            <Link to = "/tuiter/home"> Tuiter </Link> 
+            </div>
     )
 }
 
